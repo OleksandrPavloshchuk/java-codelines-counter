93e7cf4c155818e46e1833c0e31627f2fa81139f
OleksandrPavloshchuk <ap730518@gmail.com>
2020-11-27 21:57:41 +0200

    small refactoring (TODO: check for string variables)

--- src/main/java/tests/java/code/line/counter/builder/FileCounter.java
+++ src/main/java/tests/java/code/line/counter/builder/FileCounter.java
@@ -26,6 +26,10 @@ class FileCounter extends CounterBase {
     private ParserState parserState = ParserState.CODE;
     // Current string:
     private String str;
+    // Current position in the string:
+    private int index;
+    // Is current line contains code?
+    private boolean lineContainsCode;
     // Counter
     private int count = 0;
 
@@ -63,30 +67,24 @@ class FileCounter extends CounterBase {
             parserState = ParserState.CODE;
         }
         // Check if line starts with code
-        boolean lineContainsCode = parserState == ParserState.CODE;
+        lineContainsCode = parserState == ParserState.CODE;
         if (isTailLongerThan2(0)) {
 
             // Check line for switches:
-            for (int i = 0; i <= str.length() - 2; i++) {
-                if (isEndOfMultilineComment(i)) {
+            for (index = 0; index <= str.length() - 2; index++) {
+                if (isEndOfMultilineComment()) {
                     parserState = ParserState.CODE;
-                    if (i == 0) {
-                        lineContainsCode = false;
-                    }
-                    // Skip the multiline comment end
-                    i++;
-                } else if (isStartOfMultilineComment(i)) {
+                    setNoCodeAtStart();
+                    skipCommentMarker();
+                } else if (isStartOfMultilineComment()) {
                     parserState = ParserState.COMMENT_MULTILINE;
-                    if (i == 0) {
-                        lineContainsCode = false;
-                    }
-                    // Skip the multiline comment start
-                    i++;
-                } else if (isStartOfOneLineComment(i)) {
+                    setNoCodeAtStart();
+                    skipCommentMarker();
+                } else if (isStartOfOneLineComment()) {
                     parserState = ParserState.COMMENT_ONE_LINE;
                     // Tail of string is a comment
                     break;
-                } else if (isCode(i)) {
+                } else if (isCode()) {
                     lineContainsCode = true;
                 }
             }
@@ -97,6 +95,19 @@ class FileCounter extends CounterBase {
         }
     }
 
+    /**
+     * If line starts from comment, it does not contain code
+     */
+    private void setNoCodeAtStart() {
+        if (index == 0) {
+            lineContainsCode = false;
+        }
+    }
+
+    private void skipCommentMarker() {
+        index++;
+    }
+
     private boolean isTailLongerThan2(int i) {
         return isTailLongerThan(i, 2);
     }
@@ -105,33 +116,42 @@ class FileCounter extends CounterBase {
         return (str.length() - i) >= limit;
     }
 
-    private boolean isEndOfMultilineComment(int i) {
+    /**
+     * @return true, if this is end of multiline comment
+     */
+    private boolean isEndOfMultilineComment() {
         return parserState == ParserState.COMMENT_MULTILINE
-                && matches(i, "*/");
-    }
-
-    private boolean isStartOfMultilineComment(int i) {
-        return parserState == ParserState.CODE
-                && matches(i, "/*");
-    }
-
-    private boolean isStartOfOneLineComment(int i) {
-        return parserState == ParserState.CODE
-                && matches( i, "//");
-    }
-
-    private boolean matches(int i, String match) {
-        return str.substring(i, i + 2).equals(match);
+                && matches("*/");
     }
 
     /**
-     * Check the code text (skip blanks)
-     *
-     * @param i
-     * @return
+     * @return true, if this is start of multiline comment
      */
-    private boolean isCode(int i) {
-        final int c = str.charAt(i);
+    private boolean isStartOfMultilineComment() {
+        return parserState == ParserState.CODE && matches("/*");
+    }
+
+    /**
+     * @return true, if this is start of one line comment
+     */
+    private boolean isStartOfOneLineComment() {
+        return parserState == ParserState.CODE && matches("//");
+    }
+
+    /**
+     * @param s
+     * @return true, if next 2 characters matches string s
+     */
+    private boolean matches(String s) {
+        return str.substring(index, index + 2).equals(s);
+    }
+
+    /**
+     * @return true, if parser in the code state and current character is not
+     * blank
+     */
+    private boolean isCode() {
+        final int c = str.charAt(index);
         return parserState == ParserState.CODE
                 && c != '\t' && c != ' ';
     }
