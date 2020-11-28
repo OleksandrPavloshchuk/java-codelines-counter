62bdbef24cf7135823183fa3868b2f3f4d230e5d
OleksandrPavloshchuk <ap730518@gmail.com>
2020-11-28 10:34:58 +0200

    parser one by one character (TODO: strings and characters)

--- src/main/java/tests/java/code/line/counter/Main.java
+++ src/main/java/tests/java/code/line/counter/Main.java
@@ -7,15 +7,17 @@ import tests.java.code.line.counter.builder.Counter;
 public class Main {
   
     public static void main(String[] args) throws IOException {
+
         // By default print from the current directory:
         final String path = args.length>=1 ? args[0] : ".";
-        
-        // String path = "/home/oleksandr/workspaces/java/tests/java-codelines-counter/src/test/resources/samples/Calculator.java";
-        
+
+        // Build counter for file or directory:
         final Counter counter = CounterFactory.build(path);
         if (counter == null) {
+            // If we work with non-java file, counter is null
             System.out.println("File '" + path + "' is not java source file");
         } else {
+            // Count lines of code in the tree and print it
             new Printer().print(counter.count(), System.out);            
         }
     }
