20a730e19c88a5e5d53376f43fa0f92c1c47fe37
OleksandrPavloshchuk <ap730518@gmail.com>
2020-12-01 18:55:21 +0200

    legacy code

---
+++ jcc-java-code-line-counter-cli/src/main/java/org/jcc/java/code/line/counter/cli/Main.java
@@ -0,0 +1,25 @@
+package org.jcc.java.code.line.counter.cli;
+
+import java.io.IOException;
+import org.jcc.java.code.line.counter.Counter;
+import org.jcc.java.code.line.counter.CounterFactory;
+import org.jcc.java.code.line.counter.output.SimplePrinter;
+
+public class Main {
+  
+    public static void main(String[] args) throws IOException {
+
+        // By default print from the current directory:
+        final String path = args.length>=1 ? args[0] : ".";
+
+        // Build counter for file or directory:
+        final Counter counter = CounterFactory.build(path);
+        if (counter == null) {
+            // If we work with non-java file, counter is null
+            System.out.println("File '" + path + "' is not java source file");
+        } else {
+            // Count lines of code in the tree and print it
+            new SimplePrinter().print(counter.count(), System.out);            
+        }
+    }
+}
